add06c391ae17c2a7b2ca696490d9c4faea70442
calocaom <omarcaloca1@gmail.com>
2026-08-13 14:18:20 +0200

    Use landscape-10 for the UX/UI project hexagon cover.

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,13 +1,13 @@
 import {
   CAROUSEL_1,
-  CAROUSEL_2,
+  CAROUSEL_10,
   PROJECT_ART,
 } from '../assets'
 import { publicUrl } from '../utils/publicUrl'
 
 export const PROJECT_IMAGES = {
   1: CAROUSEL_1,
-  2: CAROUSEL_2,
+  2: CAROUSEL_10,
   4: publicUrl('portfolios/mua/06.png'),
   5: PROJECT_ART,
 }
